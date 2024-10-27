161cd997b0a997116ab0a137753cfd2f682aaa03
FR112005 <Fr253@kent.ac.uk>
2024-10-27 21:18:32 +0000

    Update WhileLoops.py

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,15 @@
 ### Part Two -- your code goes here. 
+
+import random
+correct_number = random.randint(1, 100)
+user_guess = None
+
+while user_guess != correct_number:
+    user_guess = int(input("Guess a number between 1 and 100: "))
+
+    if user_guess > correct_number:
+        print("Too high! Try again.")
+    elif user_guess < correct_number:
+        print("Too low! Try again.")
+    else:
+        print("Congratulations! You guessed the correct number: {correct_number}")
